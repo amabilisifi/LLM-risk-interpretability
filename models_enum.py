2201a34ebea98eff41f58d968cdf8277c3a95126
Amabilisfi <nzn.amabilis@gmail.com>
2025-09-06 10:15:47 +0330

    added models decisions enum

--- models_enum.py
+++ models_enum.py
@@ -6,10 +6,18 @@ class ModelEnum(Enum):
     Core: Gemma-7B, LLaMA-3-8B, Phi-4-14B.
     Stretch: LLaMA-3-13B/14B.
     '''
+     # --- Baseline ---
     GEMMA_2B = "google/gemma-2b"  
+
+    # --- Core Models ---
     GEMMA_7B = "google/gemma-7b" 
     LLAMA3_8B = "meta-llama/Meta-Llama-3-8B" 
     PHI_4 = "microsoft/phi-4" 
+
+    # --- Stretch Models (optional, if GPU allow) ---
+    LLAMA3_13B = "meta-llama/Meta-Llama-3-13B"
+    # LLAMA3_27B = "meta-llama/Meta-Llama-3-27B"
+    # LLAMA3_70B = "meta-llama/Meta-Llama-3-70B"
 
     @classmethod
     def get_model_name(cls, enum_member):
